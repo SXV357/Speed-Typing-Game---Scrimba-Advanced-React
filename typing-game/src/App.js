caaf17c8b74dddc556320bde79624518a291f643
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-06-19 23:40:10 -0500

    application setup(styles + skeleton)

--- typing-game/src/App.js
+++ typing-game/src/App.js
@@ -4,7 +4,11 @@ import './style.css';
 export default function App() {
   return (
     <div>
-
+      <h1>SPEED TYPING GAME</h1>
+      <textarea></textarea>
+      <h4>Time Remaining: </h4>
+      <button>Start Game</button>
+      <h1>Word Count: </h1>
     </div>
   );
 }
