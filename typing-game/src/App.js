55364a683377c0fbb37797839d3d87f208b85d6e
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-06-20 23:44:14 -0500

    refactor time + restart logic

--- typing-game/src/App.js
+++ typing-game/src/App.js
@@ -1,53 +1,61 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 import './style.css';
 
 export default function App() {
+  const AVAILABLE_TIME = 10;
 
   const [text, setText] = useState("");
   const [startTimer, setStartTimer] = useState(false);
   const [numWords, setNumWords] = useState(0)
-  const [time, setTime] = useState({
-    initialTime: 10,
-    modifiedTime: 10,
-  })
-  const [resetGame, setResetGame] = useState(false);
- 
-  const updateText = (e) => {
-    setText(e.target.value)
+  const [timeRemaining, setTimeRemaining] = useState(AVAILABLE_TIME);
+  const textAreaElem = useRef(null);
+
+  function updateText(e){
+    const {value} = e.target;
+    setText(value);
   }
 
-  const calculateNumWords = () => {
+  function calculateNumWords(){
     let wordArray = text.length > 0 ? text.split(" ").filter(word => word !== "") : [];
     return wordArray.length;
   }
 
+  function handleGame(){
+    setStartTimer(true);
+    if (timeRemaining === 0){
+      setText("");
+      setNumWords(0);
+      setStartTimer(true);
+      setTimeRemaining(AVAILABLE_TIME);
+    }
+  }
+
   useEffect(() => {
-    let countdownTimer = time.modifiedTime > 0 && startTimer && setTimeout(() => {
-      setTime(prevTime => ({...prevTime, modifiedTime: prevTime.modifiedTime - 1}))
+    textAreaElem.current.focus();
+  }, [startTimer])
+
+  useEffect(() => {
+    let countdownTimer = timeRemaining > 0 && startTimer && setTimeout(() => {
+      setTimeRemaining(prevRemaining => prevRemaining - 1)
     }, 1000)
-    if (time.modifiedTime === 0){
+
+    if (timeRemaining === 0){
       setStartTimer(false);
       setNumWords(calculateNumWords())
     };
+
     return () => {
       clearTimeout(countdownTimer);
     }
-  }, [time.modifiedTime, startTimer])
+  }, [timeRemaining, startTimer])
 
   return (
     <div>
       <h1>SPEED TYPING GAME</h1>
-      <textarea value = {text} name = "text" onChange = {(e) => updateText(e)}/>
-      <h4>Time Remaining: {!resetGame ? time.modifiedTime: time.initialTime}</h4>
-      <button onClick = {() => {
-        setStartTimer(true);
-        if (time.modifiedTime === 0){
-          setText("");
-          setNumWords(0);
-          setResetGame(true);
-        }
-      }}>Start Game</button>
-      <h1>Word Count: {time.modifiedTime === 0 && numWords}</h1>
+      <textarea ref = {textAreaElem} value = {text} name = "text" onChange = {(e) => updateText(e)} disabled = {!startTimer}/>
+      <h4>Time Remaining: {timeRemaining}</h4>
+      <button onClick = {handleGame} disabled = {startTimer}>Start Game</button>
+      <h1>Word Count: {timeRemaining === 0 && numWords}</h1>
     </div>
   );
 }
